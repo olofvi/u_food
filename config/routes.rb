253a3c930d0124b5ad271497cdb02cf2bb05fbb4
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-04 17:26:34 +0200

    changed restaurant controller name to restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root controller: :restaurant, action: :index
+  root controller: :restaurants, action: :index
 end
