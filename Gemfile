f8b2692d6c919728a3e70f9e7abc8ef1b0e69af2
olofvi <olofvi89@gmail.com>
2017-10-05 19:25:22 +0200

    added acts as shopping cart gem

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem "haml-rails", "~> 1.0"
+gem 'acts_as_shopping_cart', '~> 0.4.1'
 
 group :development, :test do
   gem 'pry-byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005103549) do
+ActiveRecord::Schema.define(version: 20171005172058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
