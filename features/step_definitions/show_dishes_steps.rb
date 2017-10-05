b99b6d1483d3636270cb8e2057649e5a82bf8f8b
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-05 15:16:49 +0200

    fixed the path

--- features/step_definitions/show_dishes_steps.rb
+++ features/step_definitions/show_dishes_steps.rb
@@ -5,17 +5,5 @@ Given("the following dishes exists") do |table|
 end
 
 Given("I visit the dish page") do
-  visit '/restaurants/show'
-end
-
-Then("I should see the text {string}") do |text|
-  expect(page).to have_content text
-end
-
-Then("I should see the picture {string}") do |pic_url|
-  expect(page).to have_xpath("//img[@src='#{pic_url}']")
-end
-
-And(/^show me the page$/) do
-  save_and_open_page
+  visit restaurant_path(:id)
 end
