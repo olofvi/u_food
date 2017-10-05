049f205f4fb623c04a0b290907998fc948ff4390
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-05 15:44:54 +0200

    changed feature test to use factorygirl

--- features/step_definitions/restaurant_category_steps.rb
+++ features/step_definitions/restaurant_category_steps.rb
@@ -1,6 +1,6 @@
 Given("the following table exists") do |table|
-  table.hashes.each do |item|
-    Restaurant.create!(item)
+  table.hashes.each do |hash|
+    FactoryGirl.create(:restaurant, hash)
   end
 end
 
