6fbd0076c4724b5d8957c11b813638b1561fd765
Raoul DIFFOUO <diraulo@gmail.com>
2017-10-06 15:58:55 +0200

    first attempt at getting tests to pass, needs refactoring

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,8 +2,9 @@ class CartsController < ApplicationController
   def add
     dish = Dish.find(params[:id])
     @cart = Cart.create
+    session[:cart_id] = @cart.id
     @cart.add(dish, dish.price)
-    flash[:notice] = "#{dish.name} added to order"
+    flash[:notice] = "#{dish.name} added to cart"
     redirect_to restaurant_path(:id)
   end
 end
