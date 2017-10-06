5efa8e4661f19452f4821999192be0534b9ba103
olofvi <olofvi89@gmail.com>
2017-10-06 12:03:47 +0200

    implementing acts_as_shopping_cart

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -6,6 +6,10 @@ Then("I should see the picture {string}") do |pic_url|
   expect(page).to have_xpath("//img[@src='#{pic_url}']")
 end
 
+Then("I click on {string}") do |string|
+  click_link_or_button string
+end
+
 And(/^show me the page$/) do
   save_and_open_page
 end
