b27b9e33a492552318610f243cf10b37a8c71762
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-07 18:21:27 +0200

    finding cart with session id

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   def add
     dish = Dish.find(params[:id])
     if Cart.any?
-      @cart = Cart.last
+      find_cart
     else
       @cart = Cart.create
     end
@@ -10,5 +10,9 @@ class CartsController < ApplicationController
     @cart.add(dish, dish.price)
     flash[:notice] = "#{dish.name} added to cart"
     redirect_to restaurant_path(params[:restaurant_id])
+  end
+
+  def find_cart
+    @cart = Cart.find(session[:cart_id]) if session[:cart_id]
   end
 end
