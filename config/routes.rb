7b7aa71b53fe22e406bcc6362d3a50d8f383d29c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-07 12:50:26 +0200

    migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-
-
   root controller: :restaurants, action: :index
   resources :restaurants
-
   get '/add_to_cart/:id', controller: :carts, action: :add, as: :add_to_cart
 end
