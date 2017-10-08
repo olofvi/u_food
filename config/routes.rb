92db80feaa15196356a3ff291a0d95e2e4fa25c0
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-08 14:25:19 +0200

    add cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root controller: :restaurants, action: :index
   resources :restaurants
+  resources :carts
   get '/add_to_cart/:id', controller: :carts, action: :add, as: :add_to_cart
 end
