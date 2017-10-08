82919684c15c18396c3e59c88c53770a5f73c7e4
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-08 20:01:53 +0200

    refactored feature test

--- features/step_definitions/view_restaurant_page_steps.rb
+++ features/step_definitions/view_restaurant_page_steps.rb
@@ -1,9 +1,9 @@
 Given("the following restaurants exist") do |table|
   table.hashes.each do |hash|
-    res_category = RestaurantCategory.find { |category| category.name == hash[:restaurant_category]}
+    res_category = RestaurantCategory.find_by(name: hash[:restaurant_category])
     hash.delete_if {|key, value| value == res_category.name }
     restaurant = FactoryGirl.create(:restaurant, hash)
-    res_category.update(restaurants: [restaurant])
+    restaurant.update_attribute(:restaurant_category, res_category)
   end
 end
 
