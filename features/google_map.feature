c2215f6a3b449b24eef492fabcb6a7a8336cabe5
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-09 13:04:02 +0200

    feature test for google map loading is green

--- features/google_map.feature
+++ features/google_map.feature
@@ -3,6 +3,7 @@ Feature: Show a map
   To see whats in my area
   I would like to see a map of my area
 
-Scenario:
-  Given I navigate to the index page
-  And the map has been loaded
+  @javascript
+  Scenario: User can see a map, js: true
+    Given I navigate to the index page
+    And the map has been loaded
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,17 +3,30 @@ Coveralls.wear_merged!('rails')
 require 'cucumber/rails'
 require 'capybara/poltergeist'
 
-
 ActionController::Base.allow_rescue = false
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false,
+                                    phantomjs_options: ['--ssl-protocol=tlsv1.2', '--ignore-ssl-errors=yes'])
+end
+
+# Before do
+#   Aws.config[:s3] = {stub_responses: true}
+# end
+
+Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 5
 
 begin
   DatabaseCleaner.strategy = :transaction
 rescue NameError
-  raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
+  raise 'You need to add database_cleaner to your Gemfile ' \
+        '(in the :test group) if you wish to use it.'
 end
 
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+require 'capybara/poltergeist'
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, js_error: false)
 end
