175a0b1801c45debbe21748462df70d879007096
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-10 10:48:05 +0200

    added restaurant category, made test pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008113141) do
+ActiveRecord::Schema.define(version: 20171010084453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20171008113141) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "restaurant_category_id"
+    t.text "description"
     t.index ["restaurant_category_id"], name: "index_restaurants_on_restaurant_category_id"
   end
 
