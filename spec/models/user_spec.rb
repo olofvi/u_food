36bac92033cc19211f411ac678ce00fe4c813822
olofvi <olofvi89@gmail.com>
2017-10-10 18:58:53 +0200

    rspec all green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe User, type: :model do
   describe 'DB table' do
     it {is_expected.to have_db_column :id}
     it {is_expected.to have_db_column :email}
-    it {is_expected.to have_db_column :password}
+    it {is_expected.to have_db_column :encrypted_password}
   end
 end
