364a5cdc9df4ba80514faae9996e24952be1edb1
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-10 16:58:35 +0200

    trying to pass in parameters for amount

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,13 +2,14 @@ class CartsController < ApplicationController
 
   def add
     dish = Dish.find(params[:id])
+    @quantity = params[:amount].nil? ? 1 : params[:amount]
     if session[:cart_id]
       show
     else
       @cart = Cart.create
       session[:cart_id] = @cart.id
     end
-    @cart.add(dish, dish.price)
+    @cart.add(dish, dish.price, @quantity)
     flash[:notice] = "#{dish.name} added to cart"
     redirect_to restaurant_path(params[:restaurant_id])
   end
