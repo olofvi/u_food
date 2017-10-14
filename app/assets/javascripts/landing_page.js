3d71a5a645d0eaa2d50bd87b71653e3baf3e96b8
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-14 14:17:13 +0200

    changed zoom, added first try at static restaurant address

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -1,7 +1,7 @@
 function initiateMap () {
     map = GMaps({
         div: '#map',
-        zoom: 12,
+        zoom: 15,
         lat: 57.7089,
         lng: 11.9746
     });
@@ -18,7 +18,7 @@ function performGeolocation() {
                 latitude = position.coords.latitude;
                 longitude = position.coords.longitude;
                 map.setCenter(latitude, longitude);
-                map.addMarker({
+                    map.addMarker({
                     lat: position.coords.latitude,
                     lng: position.coords.longitude,
                     title: 'You are here',
@@ -32,6 +32,24 @@ function performGeolocation() {
             },
             not_supported: function () {
                 alert('Your browser does not support geolocation');
+            }
+        });
+        GMaps.geocode({
+            // var restaurant = [{name: "Thai", address: "Valhallavägen 49 Stockholm"}]
+            address: 'valhallavägen 49 stockholm', //restaurant.address,
+            callback: function(results, status) {
+                if (status == 'OK') {
+                    var latlng = results[0].geometry.location;
+                    map.setCenter(latlng.lat(), latlng.lng());
+                    map.addMarker({
+                        lat: latlng.lat(),
+                        lng: latlng.lng(),
+                        title: 'Restaurant', //restaurant.name,
+                        infoWindow: {
+                            content: '<p>This is a restaurant!</p>'
+                        }
+                    });
+                }
             }
         });
     } else {
