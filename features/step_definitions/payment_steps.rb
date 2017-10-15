cc859e7de312c91cb849554e4fec21df770c16ea
olofvi <olofvi89@gmail.com>
2017-10-15 23:03:23 +0200

    add rspec for Cart

--- features/step_definitions/payment_steps.rb
+++ features/step_definitions/payment_steps.rb
@@ -2,19 +2,8 @@ Then('I should be on cart page') do
   cart_path(Cart.ids, Restaurant.ids)
 end
 
-# And(/^I fill in my card details $/) do
-#   sleep(0.1) until page.evaluate_script('$.active') == 0
-#   @stripe_iframe = all('iframe[name=stripe_checkout_app]').last
-#   within_frame @stripe_iframe do
-#     fill_in 'Email', with: 'random@morerandom.com'
-#     fill_in 'Card number', with: '4242 4242 4242 4242'
-#     fill_in 'CVC', with: '123'
-#     fill_in 'cc-exp', with: '12/2021'
-#   end
-# end
-
 Given('I fill in my card details') do
-  # sleep(0.1) until page.evaluate_script('$.active') == 0
+  sleep(0.1) until page.evaluate_script('$.active') == 0
   @stripe_iframe = all('iframe[name=stripe_checkout_app]').last
   within_frame @stripe_iframe do
     fill_in 'Email', with: 'random@morerandom.com'
---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'stripe_mock'
+
+describe Cart do
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
+  it "creates a stripe customer" do
+    customer = Stripe::Customer.create({
+                                           email: 'johnny@appleseed.com',
+                                           card: stripe_helper.generate_card_token
+                                       })
+    expect(customer.email).to eq('johnny@appleseed.com')
+  end
+end
