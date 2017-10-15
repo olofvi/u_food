15f2b9e568fdb116ea47057134e7c12744d17127
olofvi <olofvi89@gmail.com>
2017-10-15 15:49:26 +0200

    edit step definition

--- features/step_definitions/payment_steps.rb
+++ features/step_definitions/payment_steps.rb
@@ -3,5 +3,12 @@ Then("I should be on the {string} page") do |page|
 end
 
 When("I fill in appropriate card details") do
-  pending # Write code here that turns the phrase above into concrete actions
+  sleep(0.1) until page.evaluate_script('$.active') == 0
+  @stripe_iframe = all('iframe[name=stripe_checkout_app]').last
+  within_frame @stripe_iframe do
+    fill_in 'Email', with: 'random@morerandom.com'
+    fill_in 'Card number', with: '4242 4242 4242 4242'
+    fill_in 'CVC', with: '123'
+    fill_in 'cc-exp', with: '12/2021'
+  end
 end
