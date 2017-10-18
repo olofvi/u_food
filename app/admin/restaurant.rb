13d54eed421281abd296a04bf90c13f854a76698
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-18 11:22:26 +0200

    add migration file for admin user reference column in restaurants add associations in model files

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Restaurant do
 
-  permit_params :name, :address, :description, :restaurant_category_id
+  permit_params :name, :address, :description, :restaurant_category_id, :admin_user_id
 
 end
