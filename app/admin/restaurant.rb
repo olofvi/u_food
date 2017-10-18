d8195ab78cb6fb4c733e3c0ebf12b192d64fe36f
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-18 16:38:16 +0200

    made some minor change in AA restaurant model

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Restaurant do
 
-  permit_params :name, :address, :description, :restaurant_category_id, :admin_user_id
+  permit_params :name, :address, :description, :restaurant_category_id, :admin_user
 
   scope_to :current_user
 end
