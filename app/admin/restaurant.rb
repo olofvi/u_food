7d08dbf0c4eab3dc6dde79a99c6ae59d12474333
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-19 09:34:33 +0200

    AA restaurant hides amdmin user filter if not authorized

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -3,13 +3,6 @@ ActiveAdmin.register Restaurant do
   permit_params :name, :address, :description, :latitude, :longitude, :restaurant_category, :admin_user
 
 
-  # index do
-  #   column :name
-  #   column '' do |post|
-  #     link_to 'Edit', admin_restaurants_path(post) if authorized? :update, post
-  #   end
-  # end
-
   index do
     selectable_column
     id_column
@@ -18,15 +11,22 @@ ActiveAdmin.register Restaurant do
     column :restaurant_category, sortable: :restaurant_category
     column :admin_user, sortable: :admin_user if authorized? :manage
     column :description, sortable: :description
-    column :latitude
-    column :longitude
     actions
   end
 
   filter :name
   filter :address
   filter :restaurant_category
-  filter :admin_user
+  filter :admin_user, collection: proc {(AdminUser.all).map{|c| [c.email, c.id]}}, if: proc { authorized? :manage }
   filter :description
 
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :address
+      f.input :restaurant_category
+      f.input :description
+    end
+    f.actions
+  end
 end
