a45ccb7eb2fd223c2a23bd249cac9259166382d5
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-19 11:22:54 +0200

    created feature file for restaurant owner performs CRUD on menus

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
     else
       can :read, ActiveAdmin::Page, name: 'Dashboard'
       can :manage, Restaurant, admin_user: admin
+      can :manage, Menu, restaurant_id: admin.restaurants
     end
   end
 end
